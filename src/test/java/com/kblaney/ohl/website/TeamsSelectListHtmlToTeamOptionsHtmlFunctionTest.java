1149dddd5642e82f19807ed003039491d0e2cc82
kblaney <kblaney@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-11-23 04:09:33 +0000

    Add unit tests of TeamsSelectListHtmlToTeamOptionsHtmlFunction class.
    git-svn-id: http://svn.assembla.com/svn/OhlGamesheets/trunk@26 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ src/test/java/com/kblaney/ohl/website/TeamsSelectListHtmlToTeamOptionsHtmlFunctionTest.java
@@ -0,0 +1,41 @@
+package com.kblaney.ohl.website;
+
+import com.google.common.collect.Lists;
+import com.google.common.base.Function;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public final class TeamsSelectListHtmlToTeamOptionsHtmlFunctionTest
+{
+  private Function<String, List<String>> function;
+
+  @Before
+  public void setUp()
+  {
+    function = new TeamsSelectListHtmlToTeamOptionsHtmlFunction();
+  }
+
+  @Test
+  public void apply_noTeamOptions()
+  {
+    assertTrue(function.apply("NO_TEAM_OPTIONS_HERE").isEmpty());
+  }
+
+  @Test
+  public void apply_oneTeamOption()
+  {
+    assertEquals(Lists.newArrayList("OPTION valueBLAHBsubType=12BLAH>KYLE<"),
+            function.apply("ABC_OPTION valueBLAHBsubType=12BLAH>KYLE<DEF"));
+  }
+
+  @Test
+  public void apply_twoTeamOptions()
+  {
+    assertEquals(Lists.newArrayList("OPTION valueBLAHBsubType=12BLAH>KYLE<",
+            "OPTION valueBIGsubType=9BLAH>A>A>B<"),
+            function.apply("ABC_OPTION valueBLAHBsubType=12BLAH>KYLE<DEF\n" +
+            "DEF_OPTION valueBIGsubType=9BLAH>A>A>B<<\n"));
+  }
+}
