7ba68720aefdde60f1691182e6adad5baf6f14e8
kblaney <kblaney@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-11-27 17:09:59 +0000

    Use XmlToElementFunction in PlayerTableRowNodeToIdFunctionTest class.
    git-svn-id: http://svn.assembla.com/svn/OhlGamesheets/trunk@35 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/test/java/com/kblaney/ohl/website/PlayerTableRowNodeToIdFunctionTest.java
+++ src/test/java/com/kblaney/ohl/website/PlayerTableRowNodeToIdFunctionTest.java
@@ -1,29 +1,25 @@
 package com.kblaney.ohl.website;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.tidy.Tidy;
 import static org.junit.Assert.*;
 
 public final class PlayerTableRowNodeToIdFunctionTest
 {
-  @Test
-  public void apply()
+  private XmlToElementFunction toElementFunction;
+
+  @Before
+  public void setUp() throws Exception
   {
-    final Document doc = Tidy.createEmptyDocument();
-    final Element rootElement = doc.createElement("root");
-    final Element trElement = doc.createElement("tr");
-    final Element firstTdElement = doc.createElement("td");
-    final Element secondTdElement = doc.createElement("td");
-    final Element playerTdElement = doc.createElement("td");
-    final Element linkElement = doc.createElement("a");
-    linkElement.setAttribute("href", "blah blah blah?1234");
-    rootElement.appendChild(trElement);
-    trElement.appendChild(firstTdElement);
-    trElement.appendChild(secondTdElement);
-    trElement.appendChild(playerTdElement);
-    playerTdElement.appendChild(linkElement);
-    assertEquals("1234", new PlayerTableRowNodeToIdFunction().apply(trElement));
+    toElementFunction = new XmlToElementFunction();
+  }
+
+  @Test
+  public void apply() throws Exception
+  {
+    final Element element = toElementFunction.apply(
+          "<tr><td/><td/><td><a href=\"blah?1234\"></a></td></tr>");
+    assertEquals("1234", new PlayerTableRowNodeToIdFunction().apply(element));
   }
 }
