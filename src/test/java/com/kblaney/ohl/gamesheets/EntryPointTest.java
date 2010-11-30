38b8c98e8191a3daacc970da0b43ee539450a2b3
kblaney <kblaney@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-11-30 23:48:25 +0000

    Add unit tests for the EntryPoint class.
    git-svn-id: http://svn.assembla.com/svn/OhlGamesheets/trunk@57 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ src/test/java/com/kblaney/ohl/gamesheets/EntryPointTest.java
@@ -0,0 +1,12 @@
+package com.kblaney.ohl.gamesheets;
+
+import org.junit.Test;
+
+public final class EntryPointTest
+{
+  @Test
+  public void foo() throws Exception
+  {
+    EntryPoint.main(new String[0]);
+  }
+}
