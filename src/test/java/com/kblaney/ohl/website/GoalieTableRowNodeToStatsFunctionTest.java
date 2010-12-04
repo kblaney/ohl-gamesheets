e515ca717474af9dec0fb528bfad0865c0c76228
kblaney <kblaney@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-12-04 20:11:00 +0000

    Add unit tests for the GoalieTableRowNodeToStatsFunction class.
    git-svn-id: http://svn.assembla.com/svn/OhlGamesheets/trunk@80 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ src/test/java/com/kblaney/ohl/website/GoalieTableRowNodeToStatsFunctionTest.java
@@ -0,0 +1,27 @@
+package com.kblaney.ohl.website;
+
+import com.kblaney.ohl.GoalieStats;
+import org.junit.Test;
+import org.w3c.dom.Element;
+import static org.junit.Assert.*;
+
+public final class GoalieTableRowNodeToStatsFunctionTest
+{
+  @Test
+  public void apply() throws Exception
+  {
+    final Element tableRowNode = new XmlToDomElementFunction().apply(
+          "<tr><td/><td/><td><a>Malcolm Subban</a></td>" +
+          "<td>10</td><td>588</td><td>30</td><td>0</td><td>3.06</td" +
+          "><td>3</td><td>5</td><td>0</td><td>1</td><td>304</td>" +
+          "<td>274</td><td>0.901</td></tr>");
+    final GoalieStats expected = new GoalieStats.Builder().
+          setNumGamesPlayed(10).setNumMinutesPlayed(588).setNumGoalsAgainst(30).
+          setNumShutouts(0).setGoalsAgainstAverage(3.06).setNumWins(3).
+          setNumRegulationLosses(5).setNumOvertimeLosses(0).
+          setNumShootoutLosses(1).setNumShotsAgainst(304).setNumSaves(274).
+          setSavePercentage(0.901).build();
+    assertEquals(expected, new GoalieTableRowNodeToStatsFunction().apply(
+          tableRowNode));
+  }
+}
