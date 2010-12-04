8af818cd3bf22101c145daa21e9ecab1d1409511
kblaney <kblaney@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-12-04 19:54:18 +0000

    Add unit tests for the GoalieTableRowNodeListSupplier class.
    git-svn-id: http://svn.assembla.com/svn/OhlGamesheets/trunk@79 e0e46c49-be69-4f5a-ad62-21024a331aea

--- src/test/java/com/kblaney/ohl/website/GoalieTableRowNodeListSupplierTest.java
+++ src/test/java/com/kblaney/ohl/website/GoalieTableRowNodeListSupplierTest.java
@@ -1,5 +1,6 @@
 package com.kblaney.ohl.website;
 
+import java.net.URL;
 import com.kblaney.commons.xml.UrlToDomDocumentFunction;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,19 +9,56 @@ import static org.mockito.Mockito.*;
 
 public final class GoalieTableRowNodeListSupplierTest
 {
+  private int teamNum;
+  private URL url;
   private UrlToDomDocumentFunction urlToDomDocumentFunction;
   private GoalieTableRowNodeListSupplier supplier;
 
   @Before
-  public void setUp()
+  public void setUp() throws Exception
   {
+    teamNum = 2;
+    url = Urls.getGoalieStatsUrl(teamNum);
     urlToDomDocumentFunction = mock(UrlToDomDocumentFunction.class);
     supplier = new GoalieTableRowNodeListSupplier(urlToDomDocumentFunction);
   }
 
-  @Test
-  public void get_twoGoalies()
+  @Test(expected = IllegalStateException.class)
+  public void get_noTableNode() throws Exception
   {
-    assertTrue(true);
+    when(urlToDomDocumentFunction.apply(url)).thenReturn(
+          new XmlToDomDocumentFunction().apply(
+          "<a>" + getTableHeaderRow() + getGoalieTableRow() + "</a>"));
+    supplier.get(teamNum).getLength();
+  }
+
+  private String getTableHeaderRow()
+  {
+    return "<tr><th/><th/><th/><th>SVS</th></tr>";
+  }
+
+  private String getGoalieTableRow()
+  {
+    return "<tr><td/><td/><td><a/></td></tr>";
+  }
+
+  @Test
+  public void get_zeroGoalies() throws Exception
+  {
+    when(urlToDomDocumentFunction.apply(url)).thenReturn(
+          new XmlToDomDocumentFunction().apply(
+          "<table>" + getTableHeaderRow() + "</table>"));
+    assertEquals(0, supplier.get(teamNum).getLength());
+  }
+
+  @Test
+  public void get_twoGoalies() throws Exception
+  {
+    when(urlToDomDocumentFunction.apply(url)).thenReturn(
+          new XmlToDomDocumentFunction().apply(
+          "<table>" + getTableHeaderRow() +
+          getGoalieTableRow() + getGoalieTableRow() +
+          "</table>"));
+    assertEquals(2, supplier.get(teamNum).getLength());
   }
 }
