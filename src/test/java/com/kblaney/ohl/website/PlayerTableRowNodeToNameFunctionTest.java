434e57d17d370f0c25468a4c89c0b4727230f646
kblaney <kblaney@e0e46c49-be69-4f5a-ad62-21024a331aea>
2010-12-06 05:12:39 +0000

    Add unit tests for the PlayerTableRowNodeToName class.
    git-svn-id: http://svn.assembla.com/svn/OhlGamesheets/trunk@88 e0e46c49-be69-4f5a-ad62-21024a331aea

---
+++ src/test/java/com/kblaney/ohl/website/PlayerTableRowNodeToNameFunctionTest.java
@@ -0,0 +1,15 @@
+package com.kblaney.ohl.website;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public final class PlayerTableRowNodeToNameFunctionTest
+{
+  @Test
+  public void apply() throws Exception
+  {
+    assertEquals("Stephen Silas", new PlayerTableRowNodeToNameFunction().apply(
+          new XmlToDomElementFunction().apply(
+          "<tr><td/><td/><td><a>Stephen Silas</a></td></tr>")));
+  }
+}
